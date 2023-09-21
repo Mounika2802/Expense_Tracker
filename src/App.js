0e64b61279d0d2c7e1350757a3c7fcf6cff8f53b
Mounika2802 <84626311+Mounika2802@users.noreply.github.com>
2023-09-21 12:23:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header  from './components/header';
+import Balance from './components/balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import {GlobalProvider} from './context/globalstate';
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <h2>
+      Money Minder App
+    </h2>
+  )
+}
+export default header;
+
